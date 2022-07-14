db5d13c9d70f124303623bd325ea0b1fe1b87551
Christwide Oscar <triple101.games@gmail.com>
2022-07-14 14:56:23 -0500

    changes

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,26 +1,16 @@
 import React from "react";
-import {DCard,DCard_Button,DCard_Description,DCard_Flap1,DCard_Flap2,DCard_Img,DCard_Title} from './eventsElements';
 
 const Events = () => {
   return (
     <>
       <h1>Welcome to the Testing ground for our components</h1>
-     <DCard>
-      <DCard_Img src='https://images.unsplash.com/photo-1606787366850-de6330128bfc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt='Food Platter' />
-      <DCard_Title>
-        <DCard_Button>More</DCard_Button>
-        <h1>Card Title</h1>
-        <small>Info is from everywhere</small>
-      </DCard_Title>
-      <DCard_Flap1>
-        <DCard_Description>
-        This grid is an attempt to make something nice that works on touch devices. Ignoring hover states when they're not available etc.
-        </DCard_Description>
-        <DCard_Flap2>
-          <DCard_Button>Read More</DCard_Button>
-        </DCard_Flap2>
-      </DCard_Flap1>
-     </DCard>
+     <Dcard>
+      <DcardImage src={} alt={}>
+        <DcardTitle>
+          <h1>Company Title</h1>
+        </DcardTitle>
+        <DcardInfo>Random Infomation about companies and How they are recovering. </DcardInfo>
+     </Dcard>
       
     </>
   );
